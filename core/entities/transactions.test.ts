f8ffaf01c64d8f3798158a2a737035f7cc311fcf
lucastoll <lucasfernandestolotto@gmail.com>
2024-01-09 15:01:47 -0300

    feat(transaction): add business rules and more unit tests

--- core/entities/transactions.test.ts
+++ core/entities/transactions.test.ts
@@ -1,22 +1,79 @@
 import Transaction from './transactions';
 
 describe('Transaction entity', () => {
+	const transaction = new Transaction(
+		'1',
+		'Test transaction',
+		'Test description',
+		'completed',
+		100,
+		'2021-12-01',
+		'Account A',
+		'Account B',
+	);
+
 	it('should create a transaction with given values', () => {
-		const transaction = new Transaction();
-		transaction.id = 1;
-		transaction.description = 'Test transaction';
-		transaction.status = 'completed';
-		transaction.amount = 100;
-		transaction.date = '2021-12-01';
-		transaction.from = 'Account A';
-		transaction.to = 'Account B';
-        
-		expect(transaction.id).toBe(1);
-		expect(transaction.description).toBe('Test transaction');
+		expect(transaction.id).toBeDefined();
+        expect(transaction.title).toBe('Test transaction');
+		expect(transaction.description).toBe('Test description');
 		expect(transaction.status).toBe('completed');
 		expect(transaction.amount).toBe(100);
 		expect(transaction.date).toBe('2021-12-01');
 		expect(transaction.from).toBe('Account A');
 		expect(transaction.to).toBe('Account B');
+	});
+
+	it('should throw an error for empty ID on set', () => {
+		expect(() => {
+			transaction.id = '';
+		}).toThrowError('Property ID cannot be empty');
+	});
+
+    it('should throw an error for empty description on set', () => {
+		expect(() => {
+			transaction.description = '';
+		}).toThrowError('Property description cannot be empty');
+	});
+
+    it('should throw an error for empty title on set', () => {
+		expect(() => {
+			transaction.title = '';
+		}).toThrowError('Property title cannot be empty');
+	});
+
+    it('should throw an error for empty status on set', () => {
+		expect(() => {
+			transaction.status = '';
+		}).toThrowError('Property status cannot be empty');
+	});
+
+    it('should throw an error for empty date on set', () => {
+		expect(() => {
+			transaction.date = '';
+		}).toThrowError('Property date cannot be empty');
+	});
+
+    it('should throw an error for dates not in the YYYY-MM-DD format', () => {
+		expect(() => {
+			transaction.date = '09-01-2024';
+		}).toThrowError('Invalid date format. Expected format: YYYY-MM-DD');
+	});
+
+    it('should throw an error for empty from on set', () => {
+		expect(() => {
+			transaction.from = '';
+		}).toThrowError('Property from cannot be empty');
+	});
+
+    it('should throw an error for empty to on set', () => {
+		expect(() => {
+			transaction.to = '';
+		}).toThrowError('Property to cannot be empty');
+	});
+
+    it('should throw an error for amounts equal or less than 0 on set', () => {
+		expect(() => {
+			transaction.amount = 0;
+		}).toThrowError('Property amount cannot be less than or equal to 0');
 	});
 });
--- core/entities/transactions.ts
+++ core/entities/transactions.ts
@@ -1,22 +1,123 @@
 export default class Transaction {
-	constructor() {
-		this.id = 0;
-		this.description = '';
-		this.title = '';
-		this.status = '';
-		this.amount = 0;
-		this.date = '';
-		this.from = '';
-		this.to = '';
+	private _id: string;
+	private _title: string;
+	private _description: string;
+	private _status: string;
+	private _amount: number;
+	private _date: string;
+	private _from: string;
+	private _to: string;
+
+	constructor(
+		id: string,
+		title: string,
+		description: string,
+		status: string,
+		amount: number,
+		date: string,
+		from: string,
+		to: string,
+	) {
+		this._id = id;
+		this._title = title;
+		this._description = description;
+		this._status = status;
+		this._amount = amount;
+		this._date = date;
+		this._from = from;
+		this._to = to;
 	}
 
-	id: number;
-	title: string;
-	description: string;
-	status: string;
-	amount: number;
-	date: string;
-	from: string;
-	to: string;
+	get id(): string {
+		return this._id;
+	}
+
+	set id(value: string) {
+		if (value === '') {
+			throw new Error('Property ID cannot be empty');
+		}
+		this._id = value;
+	}
+
+	get title(): string {
+		return this._title;
+	}
+
+	set title(value: string) {
+		if (value === '') {
+			throw new Error('Property title cannot be empty');
+		}
+		this._title = value;
+	}
+
+	get description(): string {
+		return this._description;
+	}
+
+	set description(value: string) {
+		if (value === '') {
+			throw new Error('Property description cannot be empty');
+		}
+		this._description = value;
+	}
+
+	get status(): string {
+		return this._status;
+	}
+
+	set status(value: string) {
+		if (value === '') {
+			throw new Error('Property status cannot be empty');
+		}
+		this._status = value;
+	}
+
+	get amount(): number {
+		return this._amount;
+	}
+
+	set amount(value: number) {
+		if (value <= 0) {
+			throw new Error('Property amount cannot be less than or equal to 0');
+		}
+		this._amount = value;
+	}
+
+	get date(): string {
+		return this._date;
+	}
+
+set date(value: string) {
+    if (value === '') {
+        throw new Error('Property date cannot be empty');
+    }
+
+    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dateRegex.test(value)) {
+        throw new Error('Invalid date format. Expected format: YYYY-MM-DD');
+    }
+
+    this._date = value;
 }
-[];
+	get from(): string {
+		return this._from;
+	}
+
+	set from(value: string) {
+		if (value === '') {
+			throw new Error('Property from cannot be empty');
+		}
+		this._from = value;
+	}
+
+	get to(): string {
+		return this._to;
+	}
+
+	set to(value: string) {
+		if (value === '') {
+			throw new Error('Property to cannot be empty');
+		}
+		this._to = value;
+	}
+}
