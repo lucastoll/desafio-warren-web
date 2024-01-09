33edfb950baaa4a4a13eed0c599b9a95c89e18c4
lucastoll <lucasfernandestolotto@gmail.com>
2024-01-09 14:25:08 -0300

    feat: add eslint and prettier

--- core/entities/transactions.ts
+++ core/entities/transactions.ts
@@ -1,21 +1,22 @@
-export default class Transaction{
-    constructor() {
-        this.id = 0
-        this.description = '';
-        this.title = '';
-        this.status = '';
-        this.amount = 0;
-        this.date = '';
-        this.from = '';
-        this.to = '';
-    }
+export default class Transaction {
+	constructor() {
+		this.id = 0;
+		this.description = '';
+		this.title = '';
+		this.status = '';
+		this.amount = 0;
+		this.date = '';
+		this.from = '';
+		this.to = '';
+	}
 
-    id: number;
-    title: string;
-    description: string;
-    status: string;
-    amount: number;
-    date: string;
-    from: string;
-    to: string;
-}[];
+	id: number;
+	title: string;
+	description: string;
+	status: string;
+	amount: number;
+	date: string;
+	from: string;
+	to: string;
+}
+[];
