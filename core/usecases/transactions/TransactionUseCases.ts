56d1d7375280d0f6a9b0a94646464ee663f27a1f
lucastoll <lucasfernandestolotto@gmail.com>
2024-01-09 17:57:45 -0300

    feat: add getById and unit tests

--- core/usecases/transactions/TransactionUseCases.ts
+++ core/usecases/transactions/TransactionUseCases.ts
@@ -1,39 +1,50 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import Transaction from '../../entities/transactions/transactions';
 import { baseURL } from '../../config';
 
 const api = axios.create({
-    baseURL: baseURL,
+	baseURL: baseURL,
 });
 
 export default class TransactionUseCases {
-    async get(): Promise<Transaction[]> {
-        try {
-            const response = await api.get('/transactions');
-            return response.data;
-        } catch (error: any) {
-            throw new Error(`HTTP error! status: ${error.response.status}`);
-        }
-    }
+	async get(): Promise<Transaction[]> {
+		try {
+			const response = await api.get('/transactions');
+			return response.data;
+		} catch (error: any) {
+			throw new Error(`HTTP error! status: ${error.response?.status}`);
+		}
+	}
 
-    filterByStatus(transactions: Transaction[], status: string): Transaction[] {
-        if(status != "created" && status != "processing" && status != "processed"){
-            throw new Error("Status must be created, processing or processed")
-        } 
+	async getById(id: string): Promise<Transaction[]> {
+		try {
+			const response = await api.get(`/transactions/${id}`);
+			return response.data;
+		} catch (error: any) {
+			throw new Error(`HTTP error! status: ${error.response.status}`);
+		}
+	}
 
-        return transactions.filter(transaction => transaction.status === status);
-    }
+	filterByStatus(transactions: Transaction[], status: string): Transaction[] {
+		if (status != 'created' && status != 'processing' && status != 'processed') {
+			throw new Error('Status must be created, processing or processed');
+		}
 
-    filterByDate(transactions: Transaction[]): Transaction[] {
-        return transactions.sort((a, b) => {
-            return new Date(b.date).getTime() - new Date(a.date).getTime();
-        });
-    }	
+		return transactions.filter(transaction => transaction.status === status);
+	}
 
-    search(transactions: Transaction[], searchTerm: string): Transaction[] {
-        return transactions.filter(transaction => {
-            return transaction.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                transaction.description.toLowerCase().includes(searchTerm.toLowerCase());
-        });
-    }
+	filterByDate(transactions: Transaction[]): Transaction[] {
+		return transactions.sort((a, b) => {
+			return new Date(b.date).getTime() - new Date(a.date).getTime();
+		});
+	}
+
+	search(transactions: Transaction[], searchTerm: string): Transaction[] {
+		return transactions.filter(transaction => {
+			return (
+				transaction.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		});
+	}
 }
--- core/usecases/transactions/TransactionsUseCase.test.ts
+++ core/usecases/transactions/TransactionsUseCase.test.ts
@@ -10,59 +10,63 @@ describe('TransactionUseCases', () => {
 		transactionUseCases = new TransactionUseCases();
 	});
 
-    describe('get', () => {
-        it('should return transactions when the response is ok', async () => {
-            nock(baseURL).get('/transactions').reply(200, mockTransactions);
-    
-            const transactions = await transactionUseCases.get();
-    
-            expect(transactions).toEqual(mockTransactions);
-        });
-    
-        it('should throw an error when the response is not ok', async () => {
-            nock(baseURL).get('/transactions').reply(400);
-    
-            await expect(transactionUseCases.get()).rejects.toThrow(
-                'HTTP error! status: 400',
-            );
-        });
-    })
+	describe('get', () => {
+		it('should return transactions when the response is ok', async () => {
+			nock(baseURL).get('/transactions').reply(200, mockTransactions);
 
-    describe('filterByStatus', () => {
-        it('should return transactions with the given status', () => {
-            const transactions = transactionUseCases.filterByStatus(
-                mockTransactions,
-                'created',
-            );
-    
-            expect(transactions.length).toBe(2);
-            expect(transactions[0].status).toBe('created');
-            expect(transactions[1].status).toBe('created');
-        });
+			const transactions = await transactionUseCases.get();
 
-        it('should throw an error when the status is invalid', () => {
-            expect(() => {
-                transactionUseCases.filterByStatus(mockTransactions, 'invalid');
-            }).toThrowError('Status must be created, processing or processed');
-        });
-    })
+			expect(transactions).toEqual(mockTransactions);
+		});
 
-    describe('filterByDate', () => {
-        it('should return transactions sorted by date', () => {
-            const transactions = transactionUseCases.filterByDate(mockTransactions);
-    
-            expect(transactions[0].date).toBe('2018-12-22');
-            expect(transactions[1].date).toBe('2017-07-23');
-            expect(transactions[2].date).toBe('2016-08-25');
-        });
-    });
+		it('should throw an error when the response is not ok', async () => {
+			nock(baseURL).get('/transactions').reply(400);
 
-    describe('search', () => {
-        it('should return transactions that match the search term', () => {
-            const transactions = transactionUseCases.search(mockTransactions, 'Resgate');
-    
-            expect(transactions.length).toBe(1);
-            expect(transactions[0].title).toBe('Resgate');
-        });
-    });
+			await expect(transactionUseCases.get()).rejects.toThrow('HTTP error! status: 400');
+		});
+	});
+
+	describe('getById', () => {
+		it('should return the transaction with the given id', async () => {
+			const id = '5f89f9f257fe42957bf6dbfd';
+			nock(baseURL).get(`/transactions/${id}`).reply(200, mockTransactions[0]);
+			const transaction = await transactionUseCases.getById(id);
+			expect(transaction).toEqual(mockTransactions[0]);
+		});
+	});
+
+	describe('filterByStatus', () => {
+		it('should return transactions with the given status', () => {
+			const transactions = transactionUseCases.filterByStatus(mockTransactions, 'created');
+
+			expect(transactions.length).toBe(2);
+			expect(transactions[0].status).toBe('created');
+			expect(transactions[1].status).toBe('created');
+		});
+
+		it('should throw an error when the status is invalid', () => {
+			expect(() => {
+				transactionUseCases.filterByStatus(mockTransactions, 'invalid');
+			}).toThrowError('Status must be created, processing or processed');
+		});
+	});
+
+	describe('filterByDate', () => {
+		it('should return transactions sorted by date', () => {
+			const transactions = transactionUseCases.filterByDate(mockTransactions);
+
+			expect(transactions[0].date).toBe('2018-12-22');
+			expect(transactions[1].date).toBe('2017-07-23');
+			expect(transactions[2].date).toBe('2016-08-25');
+		});
+	});
+
+	describe('search', () => {
+		it('should return transactions that match the search term', () => {
+			const transactions = transactionUseCases.search(mockTransactions, 'Resgate');
+
+			expect(transactions.length).toBe(1);
+			expect(transactions[0].title).toBe('Resgate');
+		});
+	});
 });
