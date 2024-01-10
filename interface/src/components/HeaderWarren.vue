121ae2858d5ef10197a6b0ab2356953d9464056f
lucastoll <lucasfernandestolotto@gmail.com>
2024-01-10 15:45:39 -0300

    feat: add header component

---
+++ interface/src/components/HeaderWarren.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <h1>
+            <img class="header__logo" src="../assets/images/logo.svg" alt="Warren investimentos">
+        </h1>
+    </header>
+</template>
+
+<style scoped lang="less">
+    @import "../assets/css/variables.less";
+
+    .header {
+        background-color: @primary;
+        padding: 16px;
+        display: flex;
+        justify-content: center;
+
+        @media (min-width: @screen-lg){
+            justify-content: flex-start;
+        }
+    }
+
+    .header__logo{
+        height: 50px;
+    }
+</style>
